55349d9293e45a96f396aab7369501d24e625731
javiera-ro <javiera.rojas72@inacapmail.cl>
2025-08-21 12:50:37 -0400

    tarea 1

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -15,15 +15,43 @@ def inicio(request):
 
 def panel_dispositivos(request):
     dispositivos = [
-        {"nombre": "Sensor Temperatura", "consumo": 50},
-        {"nombre": "Medidor Solar", "consumo": 120},
-        {"nombre": "Sensor Movimiento", "consumo": 30},
-        {"nombre": "Calefator", "consumo": 200},
+        {
+        "nombre": "Sensor Temperatura",
+        "consumo": 50,
+        "limite": 100
+        },
+
+        {"nombre": "Medidor Solar",
+         "consumo": 120,
+         "limite": 100
+        },
+
+        {"nombre": "Sensor Movimiento",
+         "consumo": 30,
+         "limite": 100
+        },
+
+        {"nombre": "Calefactor",
+         "consumo": 200,
+         "limite": 150
+        },
     ]
 
+    print("=== DEBUG INICIO ===")
+    print("Dispositivos:", dispositivos)  # ← Esto aparecerá en la TERMINAL
+    print("=== DEBUG FIN ===")
+
+    for dispositivo in dispositivos:
+        if dispositivo['consumo'] > dispositivo ['limite']:
+            dispositivo['estado']= 'Excede el límite'
+            dispositivo['clase_css'] = 'rojo'
+        else:
+            dispositivo['estado'] = "Dentro del límite"
+            dispositivo['clase_css'] = 'verde'
+ 
     consumo_maximo = 100
 
     return render(request, "dispositivos/panel.html", {
         "dispositivos": dispositivos,
-        "consumo_maximo": consumo_maximo
+        "consumo_maximo": 100
     })
